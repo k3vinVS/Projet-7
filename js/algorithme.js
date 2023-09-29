3bc0a42d15a24162178706ac1815361a6b98daf5
KEVIN VAN SCHELLEBECK <kevin.vanschellebeck@gmail.com>
2023-09-29 17:24:57 +0200

    modification noResult dans algorithme.js

--- js/algorithme.js
+++ js/algorithme.js
@@ -88,8 +88,8 @@ function sortRecipes(arrayOfItems, currentRecipeArray) {
   // TEXTE SI PAS DE RECETTES -------------------------------------------------------
   if (recettes.length === 0) {
     // console.log("pas de recette");
-    noResult.innerHTML = `<h4 class='text-center text-warning'>Aucune recette ne contient '${inputSearch.value}'.</br> Vous pouvez
-    chercher « tarte aux pommes », « poisson », etc...</h4>`;
+    noResult.innerHTML = `<h4 class='text-center text-warning'>Aucune recette ne correspond pas à votre critère... vous pouvez
+    chercher « tarte aux pommes », « poisson », etc.</h4>`;
     cardSection.innerHTML = "";
   }
 }
